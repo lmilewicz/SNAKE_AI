2ae01749ddb7ff673a7c0c243531a124126bcbf4
lmilewicz <luke21sc@gmail.com>
2020-01-11 00:19:13 +0100

    Changing Snake vision

--- snake_game.py
+++ snake_game.py
@@ -4,9 +4,13 @@ import curses
 from random import randint
 import numpy as np
 
+
 class SnakeGame:
-    def __init__(self, board_width = 20, board_height = 20, gui = False):
+    def __init__(self, board_width=20, board_height=20, gui=False):
+        self.snake = []
+        self.food = []
         self.score = 0
+        self.win = None
         self.done = False
         self.board = {'width': board_width, 'height': board_height}
         self.gui = gui
@@ -15,16 +19,15 @@ class SnakeGame:
         self.snake_init()
         self.generate_food()
         if self.gui: self.render_init()
-        #return self.generate_observations()
+        # return self.generate_observations()
         return self.input_coordinates()
 
     def snake_init(self):
-        #x = randint(5, self.board["width"] - 5)
-        #y = randint(5, self.board["height"] - 5)
+        # x = randint(5, self.board["width"] - 5)
+        # y = randint(5, self.board["height"] - 5)
         x = 10
         y = 10
-        self.snake = []
-        vertical = 0 == 0#randint(0,1) == 0
+        vertical = 0 == 0  # randint(0,1) == 0
         point = [x, y] if vertical else [x, y]
         for i in range(3):
             point = [x + i, y] if vertical else [x, y + i]
@@ -32,7 +35,7 @@ class SnakeGame:
 
     def generate_food(self):
         food = []
-        while food == []:
+        while not food:
             food = [randint(1, self.board["width"]), randint(1, self.board["height"])]
             if food in self.snake: food = []
         self.food = food
@@ -58,12 +61,40 @@ class SnakeGame:
                 self.win.addch(point[0], point[1], 'x')
         self.win.getch()
 
+    # def step(self, key):
+    #     # 0 - UP
+    #     # 1 - RIGHT
+    #     # 2 - DOWN
+    #     # 3 - LEFT
+    #     if self.done: return self.end_game()
+    #     self.create_new_point(key)
+    #     if self.food_eaten():
+    #         self.score += 1
+    #         self.generate_food()
+    #     else:
+    #         self.remove_last_point()
+    #         self.check_collisions()
+    #     if self.gui: self.render()
+    #     # return self.generate_observations()
+    #     return [self.input_coordinates(), -1]
+    #
+    # def create_new_point(self, key):
+    #     new_point = [self.snake[0][0], self.snake[0][1]]
+    #     if key == 0:
+    #         new_point[0] -= 1
+    #     elif key == 1:
+    #         new_point[1] += 1
+    #     elif key == 2:
+    #         new_point[0] += 1
+    #     elif key == 3:
+    #         new_point[1] -= 1
+    #     self.snake.insert(0, new_point)
+
     def step(self, key):
-        # 0 - UP
-        # 1 - RIGHT
-        # 2 - DOWN
-        # 3 - LEFT
-        if self.done == True: return self.end_game()
+        # 0 - LEFT
+        # 1 - CONTINUE
+        # 2 - RIGHT
+        if self.done: return self.end_game()
         self.create_new_point(key)
         if self.food_eaten():
             self.score += 1
@@ -72,22 +103,45 @@ class SnakeGame:
             self.remove_last_point()
             self.check_collisions()
         if self.gui: self.render()
-            #return self.generate_observations()
-        return [self.input_coordinates(), -1]
+        # return self.generate_observations()
+        return [self.input_coordinates(), self.score, 0]
 
     def create_new_point(self, key):
         new_point = [self.snake[0][0], self.snake[0][1]]
+        prev_point = [self.snake[1][0], self.snake[1][1]]
+
+        # direction:
+        # 0 - UP
+        # 1 - RIGHT
+        # 2 - DOWN
+        # 3 - LEFT
+        if new_point[0]-prev_point[0] < 0:
+            prev_direction = 0
+        elif new_point[1]-prev_point[1] > 0:
+            prev_direction = 1
+        elif new_point[0]-prev_point[0] > 0:
+            prev_direction = 2
+        elif new_point[1]-prev_point[1] < 0:
+            prev_direction = 3
         if key == 0:
-            new_point[0] -= 1
+            new_direction = prev_direction-1
+            if new_direction == -1:
+                new_direction = 3
         elif key == 1:
-            new_point[1] += 1
+            new_direction = prev_direction
         elif key == 2:
+            new_direction = prev_direction+1
+            if new_direction == 4:
+                new_direction = 0
+        if new_direction == 0:
+            new_point[0] -= 1
+        elif new_direction == 1:
+            new_point[1] += 1
+        elif new_direction == 2:
             new_point[0] += 1
-        elif key == 3:
+        elif new_direction == 3:
             new_point[1] -= 1
         self.snake.insert(0, new_point)
-
-                
 
     def remove_last_point(self):
         self.snake.pop()
@@ -97,33 +151,80 @@ class SnakeGame:
 
     def check_collisions(self):
         if (self.snake[0][0] == 0 or
-            self.snake[0][0] == self.board["width"] + 1 or
-            self.snake[0][1] == 0 or
-            self.snake[0][1] == self.board["height"] + 1 or
-            self.snake[0] in self.snake[1:-1]
-            or self.snake[0] == self.snake[2][:]):
+                self.snake[0][0] == self.board["width"] + 1 or
+                self.snake[0][1] == 0 or
+                self.snake[0][1] == self.board["height"] + 1 or
+                # self.snake[0] in self.snake[1:-1] or
+                self.snake[0] == self.snake[2][:]
+                ):
             self.done = True
 
+    # def input_coordinates(self):
+    #     x = [self.snake[0][0] / self.board["height"], self.snake[0][1] / self.board["width"],
+    #          (self.board["height"] - self.snake[0][0]) / self.board["height"],
+    #          (self.board["width"] - self.snake[0][1]) / self.board["width"],
+    #          (self.food[0] - self.snake[0][0]) / self.board["height"],
+    #          (self.food[1] - self.snake[0][1]) / self.board["width"],
+    #          (self.snake[len(self.snake) - 1][0] - self.snake[0][0]) / self.board["height"],
+    #          (self.snake[len(self.snake) - 1][1] - self.snake[0][1]) / self.board["width"],
+    #          (self.snake[int(len(self.snake) / 2)][0] - self.snake[0][0]) / self.board["height"],
+    #          (self.snake[int(len(self.snake) / 2)][1] - self.snake[0][1]) / self.board["width"]]
+    #     x = np.array(x)
+    #     return x.reshape(1, 10)
     def input_coordinates(self):
-        x = [self.snake[0][0]/self.board["height"], self.snake[0][1]/self.board["width"],
-             (self.board["height"] - self.snake[0][0])/self.board["height"], 
-             (self.board["width"] - self.snake[0][1])/self.board["width"],
-             (self.food[0] - self.snake[0][0])/self.board["height"],
-             (self.food[1] - self.snake[0][1])/self.board["width"],
-             (self.snake[len(self.snake)-1][0] - self.snake[0][0])/self.board["height"],
-             (self.snake[len(self.snake)-1][1] - self.snake[0][1])/self.board["width"],
-             (self.snake[int(len(self.snake)/2)][0] - self.snake[0][0])/self.board["height"],
-             (self.snake[int(len(self.snake)/2)][1] - self.snake[0][1])/self.board["width"]]
+        # 0 - distance from wall over left
+        # 1 - distance from wall ahead
+        # 2 - distance from wall over right
+        # 3, 4 - distance from apple: x/y
+        # 5 - distance from tail over left
+        # 6 - distance from tail ahead
+        # 7 - distance from tail over right
+        if self.snake[0][0] - self.snake[1][0] < 0:
+            # direction:
+            # 0 - UP
+            # 1 - RIGHT
+            # 2 - DOWN
+            # 3 - LEFT
+
+            # direction = 0
+            x = [self.snake[0][1] / self.board["width"],
+                 self.snake[0][0] / self.board["height"],
+                 (self.board["width"] - self.snake[0][1]) / self.board["width"],
+                 (self.snake[0][0] - self.food[0]) / self.board["height"],
+                 (self.food[1] - self.snake[0][1]) / self.board["width"]]
+                 # (self.snake[len(self.snake) - 1][0] - self.snake[0][0]) / self.board["height"],
+                 # (self.snake[len(self.snake) - 1][1] - self.snake[0][1]) / self.board["width"],
+                 # (self.snake[int(len(self.snake) / 2)][0] - self.snake[0][0]) / self.board["height"],
+                 # (self.snake[int(len(self.snake) / 2)][1] - self.snake[0][1]) / self.board["width"]]
+        elif self.snake[0][1] - self.snake[1][1] > 0:
+            # direction = 1
+            x = [self.snake[0][0] / self.board["height"],
+                 (self.board["width"] - self.snake[0][1]) / self.board["width"],
+                 (self.board["height"] - self.snake[0][0]) / self.board["height"],
+                 (self.food[1] - self.snake[0][1]) / self.board["width"],
+                 (self.food[0] - self.snake[0][0]) / self.board["height"]]
+        elif self.snake[0][0] - self.snake[1][0] > 0:
+            # direction = 2
+            x = [(self.board["width"] - self.snake[0][1]) / self.board["width"],
+                 (self.board["height"] - self.snake[0][0]) / self.board["height"],
+                 self.snake[0][1] / self.board["width"],
+                 (self.food[0] - self.snake[0][0]) / self.board["height"],
+                 (self.snake[0][1] - self.food[1]) / self.board["width"]]
+        elif self.snake[0][1] - self.snake[1][1] < 0:
+            # direction = 3
+            x = [(self.board["height"] - self.snake[0][0]) / self.board["height"],
+                 self.snake[0][1] / self.board["width"],
+                 self.snake[0][0] / self.board["height"],
+                 (self.snake[0][1] - self.food[1]) / self.board["width"],
+                 (self.snake[0][0] - self.food[0]) / self.board["height"]]
         x = np.array(x)
-        return x.reshape(1, 10)
-    
-    #def generate_observations(self):
+        return x.reshape(1, 5)
+
+    # def generate_observations(self):
     #    return self.done, self.score, self.snake, self.food
 
-    def render_destroy(self):
-        curses.endwin()
-
     def end_game(self):
-        if self.gui: self.render_destroy()
-        #raise Exception("Game over")
-        return [0, self.score]
+        if self.gui:
+            curses.endwin()
+        # raise Exception("Game over")
+        return [0, self.score, 1]
--- snake_main.py
+++ snake_main.py
@@ -6,68 +6,91 @@ import time
 import sys
 
 from snake_game import SnakeGame
-from snake_nn import (SnakeNetwork, model_crossover)
+from snake_nn import *
 
-def printProgress(name, i, n, start):
-    sys.stdout.write('\r%s Progress: %0.2f percent. Execution time: %0.2f' % (name, 100*(i+1)/n, time.time() - start))
+
+def print_progress(name, i, n, start):
+    sys.stdout.write(
+        "\r%s progress: %0.2f percent. Execution time: %0.2f" % (name, 100 * (i + 1) / n, time.time() - start))
     sys.stdout.flush()
-    
-def generateNewPopulation(n):
-    snakesPopulation = []
+
+
+def generate_new_population(n):
+    snakes_population = n*[None]
     start = time.time()
     for i in range(n):
-        snakesPopulation.append(SnakeNetwork())
-        printProgress("generateNewPopulation", i, n, start)
+        snakes_population[i] = SnakeNetwork()
+        print_progress("generateNewPopulation", i, n, start)
     print("\n")
-    return snakesPopulation
+    return snakes_population
 
-def playGame(z, n, snakesPopulation):
-    scoreArray = np.zeros(n)
+
+def play_game(z, n, snakes_population):
+    fitness_array = np.zeros(n)
     start = time.time()
-    for i, snake in enumerate(snakesPopulation): 
+    best_score = 0
+    for i, snake in enumerate(snakes_population):
         game = SnakeGame()
         x = game.start()
-        score = -999
+        prev_score = score = 0
+        prev_j = 0
         for j in range(400):
-            [x, score] = game.step(snake.predict_action(x))
-            if score >= 0: 
+            [x, score, end] = game.step(snake.predict_action(x))
+            if score > prev_score:
+                prev_score = score
+                prev_j = j
+            if end > 0 or j - 100 > prev_j:
                 break
-        scoreArray[i] = score*100 + j
-        
-        printProgress("Snake game", i, len(snakesPopulation), start)
-    print("\nBest score for generation %d: %d. Time: %0.2f\n" %  (z, np.max(scoreArray), time.time() - start))
-    
-    return scoreArray
+        if score > best_score:
+            best_score = score
+        fitness_array[i] = calculate_fitness(score, j)
+        print_progress("Snake game", i, len(snakes_population), start)
+    print("\nBest score for generation %d: %d. Fitness: %d. Median: %d. Time: %0.2f\n" %
+          (z, best_score, np.max(fitness_array), np.median(fitness_array), time.time()-start))
+
+    return fitness_array
+
 
 if __name__ == "__main__":
-    n = 20
+    n = 500
     parent1 = SnakeNetwork()
     score = 0
-    snakesPopulation = generateNewPopulation(n)
-    #while score < 2:
-    #    snakesPopulation = generateNewPopulation(n)
-    #    scoreArray = playGame(-1, n, snakesPopulation)
-    #    score = np.max(scoreArray)
-    
-    for z in range(1000):
-        scoreArray = playGame(z, n, snakesPopulation)
-        if np.max(scoreArray) <= 0:
-            print("Snake population from scratch")
-            snakesPopulation = generateNewPopulation(n)
-        else:
-            parent1 = snakesPopulation[np.argmax(scoreArray)]
-            scoreArray[np.argmax(scoreArray)] = -1
-            if np.max(scoreArray) > 0:
-                parent2 = snakesPopulation[np.argmax(scoreArray)]
-            else:
-                parent2 = snakesPopulation[randint(0, len(snakesPopulation)-1)]                
-            for child in snakesPopulation:
-                child.model.set_weights(model_crossover(parent1, parent2)[randint(0, 1)])
-                child.model.set_weights(child.model_mutate(child.model.get_weights()))
 
-        #game = SnakeGame(gui = True)
-        #x = game.start()
-        #for j in range(100):
-        #    [x, score] = game.step(parent1.predict_action(x))
-        #    if score >= 0: 
-        #        break
+    snakes_population = generate_new_population(n)
+    # while score < 2:
+    #    snakes_population = generate_new_population(n)
+    #    score_array = play_game(-1, n, snakes_population)
+    #    score = np.max(score_array)
+
+    for z in range(1000):
+        fitness_array = play_game(z, n, snakes_population)
+
+        if np.max(fitness_array) <= 0:
+            print("Snake population from scratch")
+            snakes_population = generate_new_population(n)
+        else:
+            parent1 = snakes_population[np.argmax(fitness_array)]
+            fitness_array[np.argmax(fitness_array)] = -1
+            if np.max(fitness_array) > 0:
+                parent2 = snakes_population[np.argmax(fitness_array)]
+            else:
+                parent2 = snakes_population[randint(0, len(snakes_population) - 1)]
+            for x, child in enumerate(snakes_population):
+                child.model.set_weights(model_crossover(parent1, parent2)[randint(0, 1)])
+                child.model.set_weights(model_mutate(child.model.get_weights()))
+
+        # if z % 20 == 19:
+        #     game = SnakeGame(gui=True)
+        #     x = game.start()
+        #     for j in range(100):
+        #         [x, score, end] = game.step(parent1.predict_action(x))
+        #         if end > 0:
+        #             break
+
+    # serialize model to JSON
+    model_json = parent1.model.to_json()
+    with open("model.json", "w") as json_file:
+        json_file.write(model_json)
+    # serialize weights to HDF5
+    parent1.model.save_weights("model.h5")
+    print("Saved model to disk")
